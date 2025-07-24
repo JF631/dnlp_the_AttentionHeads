d6057a19efc2ba831652d8bc7956f0e26cf32304
GOESTERN-1006130 <u17493@glogin9.usr.hpc.gwdg.de>
2025-07-24 17:19:27 +0200

    adds run_gpu_sanity_check.sh

--- setup_gwdg.sh
+++ setup_gwdg.sh
@@ -45,6 +45,8 @@ conda activate dnlp
 
 echo $CONDA_DEFAULT_ENV
 
+conda install -y numpy<2
+
 # Install packages
 conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install -y tqdm==4.66.2 requests==2.31.0 transformers==4.38.2 tensorboard==2.16.2 tokenizers==0.15.1 -c conda-forge -c huggingface
